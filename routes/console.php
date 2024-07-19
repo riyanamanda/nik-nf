65a612112b9cf5f149cfea9964c0e8dfd2e7b9d3
Riyan Amanda <ryant.n92@gmail.com>
2024-07-20 01:38:34 +0700

    feat: add preventing to run artisan regarding database

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,35 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('migrate:fresh', function () {
+    $this->comment('Please don\'t run any artisan command regarding database, it can break your database!');
+})->purpose('Preventing causing break database.');
+
+Artisan::command('migrate', function () {
+    $this->comment('Please don\'t run any artisan command regarding database, it can break your database!');
+})->purpose('Preventing causing break database.');
+
+Artisan::command('migrate:refresh', function () {
+    $this->comment('Please don\'t run any artisan command regarding database, it can break your database!');
+})->purpose('Preventing causing break database.');
+
+Artisan::command('migrate:install', function () {
+    $this->comment('Please don\'t run any artisan command regarding database, it can break your database!');
+})->purpose('Preventing causing break database.');
+
+Artisan::command('migrate:reset', function () {
+    $this->comment('Please don\'t run any artisan command regarding database, it can break your database!');
+})->purpose('Preventing causing break database.');
+
+Artisan::command('migrate:rollback', function () {
+    $this->comment('Please don\'t run any artisan command regarding database, it can break your database!');
+})->purpose('Preventing causing break database.');
+
+Artisan::command('db:seed', function () {
+    $this->comment('Please don\'t run any artisan command regarding database, it can break your database!');
+})->purpose('Preventing causing break database.');
+
+Artisan::command('db:wipe', function () {
+    $this->comment('Please don\'t run any artisan command regarding database, it can break your database!');
+})->purpose('Preventing causing break database.');
